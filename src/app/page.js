89ca8ef2220a4602e090b39bd4258bc91d537935
s3496h <s3496h@gmail.com>
2024-09-27 16:39:57 +0900

    MUI 추가

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    안녕
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex">
+          안녕
+        </div>
+      </main>
     </>
   );
 }
